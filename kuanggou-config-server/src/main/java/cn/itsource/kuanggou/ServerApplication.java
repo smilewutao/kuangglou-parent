246f4be25770e9566301a893067c841a78e85511
smilewutao <877159675@qq.com>
2019-10-10 16:54:20 +0800

    配置

---
+++ kuanggou-config-server/src/main/java/cn/itsource/kuanggou/ServerApplication.java
@@ -0,0 +1,14 @@
+package cn.itsource.kuanggou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class,args);
+    }
+}
